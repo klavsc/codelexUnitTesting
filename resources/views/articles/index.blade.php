a980b128a5e5a492c49ade2ec007434573b7faea
klavsc <klavs.cirulis@gmail.com>
2020-12-09 11:44:17 +0200

    Added tests for ArticlesController

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,37 +6,64 @@
         </a>
         <table class="table">
             <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Created at</th>
-                    <th scope="col">Updated at</th>
-                    <th scope="col"></th>
-                </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+                <th scope="col"></th>
+            </tr>
             </thead>
             <tbody>
-                @foreach($articles as $article)
-                    <tr>
-                        <th scope="row">{{ $article->id }}</th>
-                        <td>{{ $article->title }}</td>
-                        <td>{{ $article->created_at->format('Y-m-d h:i') }}</td>
-                        <td>{{ $article->updated_at->format('Y-m-d h:i') }}</td>
-                        <td>
-                            <a href="{{ route('articles.edit', $article) }}" class="btn btn-sm btn-warning">
-                                Edit
-                            </a>
-                            <form method="post" action="{{ route('articles.destroy', $article) }}" style="display: inline-block">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
-                                    Delete
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+            @foreach($articles as $article)
+                <tr>
+                    <th scope="row">{{ $article->id }}</th>
+                    <td>
+                        <a href="{{ route('articles.show', $article) }}">
+                            {{ $article->title }}
+                        </a>
+                    </td>
+                    <td>{{ $article->created_at->format('Y-m-d h:i') }}</td>
+                    <td>{{ $article->updated_at->format('Y-m-d h:i') }}</td>
+                    <td>
+                        <a href="{{ route('articles.edit', $article) }}" class="btn btn-sm btn-warning">
+                            Edit
+                        </a>
+                        <form method="post" action="{{ route('articles.destroy', $article) }}" style="display: inline-block">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
+
+    <script>
+        function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('txt').innerHTML =
+                h + ":" + m + ":" + s;
+            var t = setTimeout(startTime, 500);
+        }
+        function checkTime(i) {
+            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+            return i;
+        }
+    </script>
+    </head>
+
+    <body onload="startTime()">
+
+    <div id="txt"></div>
 
 @endsection
